cd686bdaa90e2826d454eefb949491122cb99ec5
installer <installer@gmail.com>
2025-08-19 14:57:49 -0400

    added bashlsp to nvim fr fr this time

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -32,10 +32,27 @@ vim.lsp.config['nixd'] = {
 	root_markers = { 'flake.nix', 'git' },
 }
 vim.lsp.config['jsonls'] = {
-  cmd = { 'vscode-json-language-server', '--stdio' },
-  filetypes = { 'json', 'jsonc' },
-  init_options = {
-    provideFormatter = true,
-  },
-  root_markers = { '.git' },
+	cmd = { 'vscode-json-language-server', '--stdio' },
+	filetypes = { 'json', 'jsonc' },
+	init_options = {
+		provideFormatter = true,
+	},
+	root_markers = { '.git' },
+}
+vim.lsp.config['bashls'] = {
+	cmd = { 'bash-language-server', 'start' },
+	settings = {
+		bashIde = {
+			-- Glob pattern for finding and parsing shell script files in the workspace.
+			-- Used by the background analysis features across files.
+
+			-- Prevent recursive scanning which will cause issues when opening a file
+			-- directly in the home directory (e.g. ~/foo.sh).
+			--
+			-- Default upstream pattern is "**/*@(.sh|.inc|.bash|.command)".
+			globPattern = vim.env.GLOB_PATTERN or '*@(.sh|.inc|.bash|.command)',
+		},
+	},
+	filetypes = { 'bash', 'sh' },
+	root_markers = { '.git' },
 }
