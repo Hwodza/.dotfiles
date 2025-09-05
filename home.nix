c798157d21c42afb26af66b8648314e7d2b594b8
installer <installer@gmail.com>
2025-09-05 15:28:20 -0400

    added bash to home-manager

--- home.nix
+++ home.nix
@@ -68,9 +68,39 @@
   #  /etc/profiles/per-user/henry/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
 
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      n = "nvim";
+      v = "nvim";
+      cd = "z";
+      t = "tmux";
+    };
+    bashrcExtra = ''
+      # add ohmyposh
+      eval "$(oh-my-posh init bash --config ~/.config/ohmyposh/kushal.omp.json)"
+
+      # Start ssh-agent if not already running and add github id
+      if [ -z "$SSH_AUTH_SOCK" ]; then
+        eval "$(ssh-agent -s)"
+        ssh-add ~/.ssh/id_github
+      fi
+      
+      clear
+
+      neofetch
+
+      # Add sesh keybind
+      s() {
+        sesh connect "$(sesh list | fzf)"
+      }
+
+      eval "$(zoxide init bash)"
+      '';
+  };
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
