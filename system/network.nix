73dafb10c5df97f6159769676dc406731017cb72
installer <installer@gmail.com>
2025-09-14 19:16:19 -0400

    network bs

--- system/network.nix
+++ system/network.nix
@@ -1,6 +1,12 @@
 { pkgs, ...}:
 
 {
+  services.resolved.enable = true;
+  networking.resolvconf.enable = false;
+  networking.networkmanager = {
+    enable = true;
+    dns = "systemd-resolved";
+  };
   environment.systemPackages = with pkgs; [
     networkmanager-fortisslvpn
   ];
