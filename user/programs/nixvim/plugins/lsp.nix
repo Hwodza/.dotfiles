bb839b3e9420e8e1fb64b23610cb3a4d8bde8e92
installer <installer@gmail.com>
2025-09-22 13:42:45 -0400

    fixed lsp

--- user/programs/nixvim/plugins/lsp.nix
+++ user/programs/nixvim/plugins/lsp.nix
@@ -1,70 +1,41 @@
 {
-  lsp = {
-    inlayHints.enable = true;
+  programs.nixvim = {
+    plugins = {
+      lsp = {
+        enable = true;
 
-    servers = {
-      nil_ls.enable = true;
-      basedpyright.enable = true;
-      clangd.enable = true;
-      gopls.enable = true;
-      biome.enable = true;
-      superhtml.enable = true;
-      jsonls.enable = true;
-      marksman.enable = true;
-      postgres_lsp.enable = true;
-      ruby_lsp.enable = true;
-      texlab.enable = true;
-      tinymist.enable = true;
-      tailwindcss.enable = true;
-      vtsls.enable = true;
-      astro.enable = true;
-      jdtls.enable = true;
-      rust_analyzer.enable = true;
-      lua_ls.enable = true;
+        keymaps = {
+          silent = true;
+          diagnostic = {
+            # Navigate in diagnostics
+            "<leader>k" = "goto_prev";
+            "<leader>j" = "goto_next";
+          };
+
+          lspBuf = {
+            gd = "definition";
+            gD = "references";
+            gt = "type_definition";
+            gi = "implementation";
+            K = "hover";
+            re = "rename";
+          };
+        };
+
+        servers = {
+					gopls.enable = true;
+					clangd.enable = true;
+          lua_ls.enable = true;
+          nil_ls.enable = true;
+					jsonls.enable = true;
+          pyright.enable = true;
+          pylsp.enable = true;
+          tflint.enable = true;
+        };
+      };
+      lsp-format = {
+        enable = true;
+      };
     };
-
-    luaConfig.content = # lua
-      ''
-        vim.diagnostic.config({
-          virtual_text = true,
-          signs = true,
-          underline = true,
-          update_in_insert = false,
-          severity_sort = true,
-        })
-      '';
   };
-
-  plugins.lspconfig.enable = true;
-
-  keymaps = [
-    {
-      key = "<leader>pp";
-      action = "<cmd>lua vim.lsp.buf.format()<cr>";
-    }
-    {
-      key = "gd";
-      action = "<cmd>lua vim.lsp.buf.definition()<cr>";
-    }
-    {
-      key = "gld";
-      action = "<cmd>lua vim.diagnostic.open_float()<cr>";
-    }
-    {
-      key = "gf";
-      action = "<cmd>lua vim.lsp.buf.references()<cr>";
-    }
-    {
-      key = "gt";
-      action = "<cmd>lua vim.lsp.buf.type_definition()<cr>";
-    }
-    {
-      key = "gfci";
-      action = "<cmd>lua vim.lsp.buf.incoming_calls()<cr>";
-    }
-    {
-      key = "goci";
-      action = "<cmd>lua vim.lsp.buf.outgoing_calls()<cr>";
-    }
-  ];
 }
