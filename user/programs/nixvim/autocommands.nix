390208575ddb8e630b3802eb74863cb5a3cba99a
installer <installer@gmail.com>
2025-09-24 14:41:26 -0400

    added highlight autocommand when yanking

---
+++ user/programs/nixvim/autocommands.nix
@@ -0,0 +1,20 @@
+{
+  programs.nixvim = {
+    autoCmd = [
+      # Highlight when yanking (copying) text
+      #  Try it with `yap` in normal mode
+      #  See `:help vim.hl.on_yank()`
+      {
+        event = [ "TextYankPost" ];
+        desc = "Highlight when yanking (copying) text";
+        group = "kickstart-highlight-yank";
+        callback.__raw = ''
+          function()
+            vim.hl.on_yank()
+          end
+        '';
+      }
+    ];
+  };
+}
+
