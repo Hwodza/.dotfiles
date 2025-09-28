35140ead7a06d8e7bc70d0883fc5b0d4382513d1
installer <installer@gmail.com>
2025-09-28 12:41:02 -0400

    added persistent folds

--- user/programs/nixvim/autocommands.nix
+++ user/programs/nixvim/autocommands.nix
@@ -4,6 +4,9 @@
       kickstart-highlight-yank = {
         clear = true;
       };
+      auto-view = {
+        clear = true;
+      };
     };
     autoCmd = [
       # Highlight when yanking (copying) text
@@ -16,6 +19,34 @@
         callback.__raw = ''
           function()
             vim.hl.on_yank()
+          end
+        '';
+      }
+      {
+        event = [ "BufWinLeave" "BufWritePost" "WinLeave" ];
+        desc = "Save view with mkview for real files";
+        group = "auto-view";
+        callback.__raw = ''
+          function(args)
+            if vim.b[args.buf].view_activated then vim.cmd.mkview { mods = { emsg_silent = true } } end
+          end
+        '';
+      }
+      {
+        event = [ "BufWinEnter" ];
+        desc = "Try to load file view if available and enable view saving for real files";
+        group = "auto-view";
+        callback.__raw = ''
+          function(args)
+            if not vim.b[args.buf].view_activated then
+              local filetype = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
+              local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+              local ignore_filetypes = { "gitcommit", "gitrebase", "svg", "hgcommit" }
+              if buftype == "" and filetype and filetype ~= "" and not vim.tbl_contains(ignore_filetypes, filetype) then
+                vim.b[args.buf].view_activated = true
+                vim.cmd.loadview { mods = { emsg_silent = true } }
+              end
+            end
           end
         '';
       }
