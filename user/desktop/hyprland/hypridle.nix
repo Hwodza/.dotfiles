37890764f6b0c56dabad5525b436ce9d240be174
installer <installer@gmail.com>
2025-10-04 21:59:45 -0400

    hypridle bs

--- user/desktop/hyprland/hypridle.nix
+++ user/desktop/hyprland/hypridle.nix
@@ -13,29 +13,30 @@
         after_sleep_cmd = "hyprctl dispatch dpms on";
         ignore_dbus_inhibit = false; # whether to ignore dbus-sent idle-inhibit requests (used by e.g. firefox or steam)
         ignore_systemd_inhibit = false; # whether to ignore systemd-inhibit --what=idle inhibitors
+        inhibit_screensaver = true;
       };
       listener = [
         {
           # Lock screen on lid close
-          timeout = 0;
+          # timeout = 0;
           on-lid = "hyprlock";
         }
-        {
-          # Lock the screen
-          timeout = 300;
-          on-timeout = "loginctl lock-session";
-        }
-        {
-          # Turn off screen
-          timeout = 420;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
-        }
-        {
-          # Suspend the system
-          timeout = 600;
-          on-timeout = "systemctl suspend";
-        }
+        # {
+        #   # Lock the screen
+        #   timeout = 300;
+        #   on-timeout = "loginctl lock-session";
+        # }
+        # {
+        #   # Turn off screen
+        #   timeout = 420;
+        #   on-timeout = "hyprctl dispatch dpms off";
+        #   on-resume = "hyprctl dispatch dpms on";
+        # }
+        # {
+        #   # Suspend the system
+        #   timeout = 600;
+        #   on-timeout = "systemctl suspend";
+        # }
       ];
     };
   };
