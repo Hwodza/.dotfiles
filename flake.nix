cdf647b715a739b8ad79eb4e9e8e8608ab975585
Henry <hwodza@gmail.com>
2026-01-15 18:16:27 -0500

    formatting and moving workspace2d as an overlay on nikpkgs

--- flake.nix
+++ flake.nix
@@ -12,119 +12,121 @@
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-unstable,
-    home-manager,
-    nix-index-database,
-    ...
-  } @ inputs: let
-    lib = nixpkgs.lib;
-    system = "x86_64-linux";
-    # pkgs = nixpkgs.legacyPackages.${system};
-    pkgs-options = {
-      inherit system;
-      config = {
-        allowUnfree = true;
-      };
-    };
-    # pkgs-unstable = import nixpkgs-unstable pkgs-options;
-    workspace2dOverlay = final: prev: {
-      hyprland-workspace2d =
-        inputs.hyprland-workspace2d.packages.${prev.system}.workspace2d;
-    };
-    pkgs = import nixpkgs (
-      pkgs-options
-      // {
-        overlays = [
-          workspace2dOverlay
-        ];
-      }
-    );
-    pkgs-unstable = import nixpkgs-unstable {inherit system;};
-  in {
-    nixosConfigurations = {
-      tester = nixpkgs.lib.nixosSystem {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      home-manager,
+      nix-index-database,
+      ...
+    }@inputs:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      # pkgs = nixpkgs.legacyPackages.${system};
+      pkgs-options = {
         inherit system;
-        modules = [
-          ./profiles/tester/configuration.nix
-        ];
-        specialArgs = {
-          inherit pkgs-unstable;
+        config = {
+          allowUnfree = true;
         };
       };
-      framework = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./profiles/framework/configuration.nix
-        ];
-        specialArgs = {
-          inherit pkgs-unstable;
-        };
+      # pkgs-unstable = import nixpkgs-unstable pkgs-options;
+      workspace2dOverlay = final: prev: {
+        hyprland-workspace2d = inputs.hyprland-workspace2d.packages.${prev.system}.workspace2d;
       };
-      pc = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          {nixpkgs.overlays = [workspace2dOverlay];}
-          ./profiles/pc/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
+      pkgs = import nixpkgs (
+        pkgs-options
+        // {
+          overlays = [
+            workspace2dOverlay
+          ];
+        }
+      );
+      pkgs-unstable = import nixpkgs-unstable { inherit system; };
+    in
+    {
+      nixosConfigurations = {
+        tester = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./profiles/tester/configuration.nix
+          ];
+          specialArgs = {
+            inherit pkgs-unstable;
+          };
+        };
+        framework = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./profiles/framework/configuration.nix
+          ];
+          specialArgs = {
+            inherit pkgs-unstable;
+          };
+        };
+        pc = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            { nixpkgs.overlays = [ workspace2dOverlay ]; }
+            ./profiles/pc/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
 
-            home-manager.users.henry = {
-              imports = [
-                ./profiles/pc/home.nix
-                
-          nix-index-database.homeModules.nix-index
-          {programs.nix-index-database.comma.enable = true;}
-              ];
-            };
-          }
-        ];
-        specialArgs = {
-          inherit pkgs-unstable;
+              home-manager.users.henry = {
+                imports = [
+                  ./profiles/pc/home.nix
+
+                  nix-index-database.homeModules.nix-index
+                  { programs.nix-index-database.comma.enable = true; }
+                ];
+              };
+            }
+          ];
+          specialArgs = {
+            inherit pkgs-unstable;
+          };
+        };
+      };
+      homeConfigurations = {
+        tester = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./profiles/tester/home.nix
+            nix-index-database.homeModules.nix-index
+            { programs.nix-index-database.comma.enable = true; }
+          ];
+          extraSpecialArgs = {
+            inherit pkgs-unstable;
+            inherit inputs;
+          };
+        };
+        framework = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./profiles/framework/home.nix
+            nix-index-database.homeModules.nix-index
+            { programs.nix-index-database.comma.enable = true; }
+          ];
+          extraSpecialArgs = {
+            inherit pkgs-unstable;
+            inherit inputs;
+          };
+        };
+        pc = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./profiles/pc/home.nix
+            nix-index-database.homeModules.nix-index
+            { programs.nix-index-database.comma.enable = true; }
+          ];
+          extraSpecialArgs = {
+            inherit pkgs-unstable;
+            inherit inputs;
+          };
         };
       };
     };
-    homeConfigurations = {
-      tester = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./profiles/tester/home.nix
-          nix-index-database.homeModules.nix-index
-          {programs.nix-index-database.comma.enable = true;}
-        ];
-        extraSpecialArgs = {
-          inherit pkgs-unstable;
-          inherit inputs;
-        };
-      };
-      framework = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./profiles/framework/home.nix
-          nix-index-database.homeModules.nix-index
-          {programs.nix-index-database.comma.enable = true;}
-        ];
-        extraSpecialArgs = {
-          inherit pkgs-unstable;
-          inherit inputs;
-        };
-      };
-      pc = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./profiles/pc/home.nix
-          nix-index-database.homeModules.nix-index
-          {programs.nix-index-database.comma.enable = true;}
-        ];
-        extraSpecialArgs = {
-          inherit pkgs-unstable;
-          inherit inputs;
-        };
-      };
-    };
-  };
 }
