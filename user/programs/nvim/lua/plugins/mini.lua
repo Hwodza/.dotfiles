90a5a8995544e7b5b6f0bad71d416a489210c6ed
Henry <hwodza@gmail.com>
2026-01-29 20:37:18 -0500

    updated mini to include comments

--- user/programs/nvim/lua/plugins/mini.lua
+++ user/programs/nvim/lua/plugins/mini.lua
@@ -1,7 +1,7 @@
 -- lua/plugins/mini.lua
 return {
 	{
-		'echasnovski/mini.pairs',
+		'nvim-mini/mini.pairs',
 		version = false,
 		config = function()
 			require('mini.pairs').setup()
@@ -15,17 +15,33 @@ return {
 	-- 	end
 	-- },
 	{
-		'echasnovski/mini.nvim',
+		'nvim-mini/mini.nvim',
 		config = function()
 			local statusline = require 'mini.statusline'
 			statusline.setup { use_icons = true }
 		end
 	},
 	{
-		'echasnovski/mini.surround',
+		'nvim-mini/mini.surround',
 		version = false,
 		config = function()
 			require('mini.surround').setup()
+		end
+	},
+	{
+		'nvim-mini/mini.nvim',
+		version = false,
+		config = function()
+			require('mini.comment').setup(
+				{
+					mappings = {
+						comment = "<C-_>",
+						comment_line = "<C-_>",
+						comment_visual = "<C-_>",
+						textobject = "<C-_>",
+					},
+				}
+			)
 		end
 	}
 }
