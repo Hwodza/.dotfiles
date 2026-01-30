b7a31b72abb9dd1dbfeccbecae1912a04989e8db
Henry <hwodza@gmail.com>
2026-01-29 20:43:59 -0500

    moved tmux to use mkoutofstoresymlink

--- user/programs/default.nix
+++ user/programs/default.nix
@@ -1,11 +1,13 @@
-{config, ...}: let
+{ config, ... }:
+let
   nvimPath = "${config.home.homeDirectory}/.dotfiles/user/programs/nvim";
-  # tmuxPath = "${config.home.homeDirectory}/.dotfiles/user/programs/.tmux";
-in {
+  tmuxPath = "${config.home.homeDirectory}/.dotfiles/user/programs/.tmux";
+in
+{
   imports = [
     # ./nixvim
     ./udiskie.nix
   ];
   xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink nvimPath;
-  # xdg.configFile."../.tmux".source = config.lib.file.mkOutOfStoreSymlink tmuxPath;
+  xdg.configFile."../.tmux".source = config.lib.file.mkOutOfStoreSymlink tmuxPath;
 }
