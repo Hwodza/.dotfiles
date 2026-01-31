a8d872a83fa5d882e239e577598c5089525fd3f0
Henry <hwodza@gmail.com>
2026-01-30 23:01:58 -0500

    wallpaper finally works

--- user/desktop/hyprland/services.nix
+++ user/desktop/hyprland/services.nix
@@ -10,28 +10,34 @@
       Unit = {
         Description = "Refresh wallpaper";
         After = [
-          "graphical-session.target"
-          "hyprland-session.target"
-          "hyprpaper.service"
+          # "graphical-session.target"
+          # # "hyprpaper.service"
+          # "hyprland-session.target"
+          "default.target"
         ];
-        Wants = [ "hyprpaper.service" ];
+        # Wants = [ "hyprpaper.service" ];
         PartOf = [ "hyprland-session.target" ];
       };
       Service = {
         Type = "oneshot";
         ExecStart = pkgs.writeShellScript "wallpaper-refresh" ''
-          	    mkdir -p "${config.my.variables.wallpaper-dir-path}"
-          	      ${pkgs.curl}/bin/curl -o "${config.my.variables.wallpaper-path}" -L "https://picsum.photos/3840/2160" 
-          	    hyprctl hyprpaper preload "${config.my.variables.wallpaper-path}"
-          	    hyprctl hyprpaper wallpaper ,"${config.my.variables.wallpaper-path}"
+          mkdir -p "${config.my.variables.wallpaper-dir-path}"
+          ${pkgs.curl}/bin/curl -o "${config.my.variables.wallpaper-path}" -L "https://picsum.photos/3840/2160" 
         '';
       };
-      Install.WantedBy = [ "hyprland-session.target" ];
+      Install.WantedBy = [
+        "default.target"
+        # "hyprpaper.service"
+      ];
     };
     timers.wallpaper-refresh = {
       Unit = {
         Description = "Timer for refreshing wallpaper";
-        After = [ "graphical-session.target" ];
+        After = [
+          "default.target"
+          "graphical-session.target"
+        ];
+
       };
       Timer = {
         # OnBootSec = "1min";
@@ -42,35 +48,44 @@
       Install.WantedBy = [ "timers.target" ];
     };
 
-    services.blueman-applet = {
-      Unit = {
-        Description = "Blueman bluetooth applet";
-        After = [ "graphical-session.target" ];
-      };
-      Service = {
-        Type = "exec";
-        ExecStart = "${pkgs.blueman}/bin/blueman-applet";
-        Restart = "always";
-      };
-      Install = {
-        WantedBy = [ "graphical-session.target" ];
-      };
-    };
+    # services.blueman-applet = {
+    #   Unit = {
+    #     Description = "Blueman bluetooth applet";
+    #     After = [
+    #       "default.target"
+    #       "graphical.target"
+    #     ];
+    #   };
+    #   Service = {
+    #     Type = "simple";
+    #     ExecStart = "${pkgs.blueman}/bin/blueman-applet";
+    #     Restart = "always";
+    #   };
+    #   Install = {
+    #     WantedBy = [ "default.target" ];
+    #   };
+    # };
 
-    services.nm-applet = {
-      Unit = {
-        Description = "NetworkManager system tray applet";
-        After = [ "graphical-session.target" ];
-      };
-      Service = {
-        Type = "exec";
-        ExecStart = "${pkgs.networkmanagerapplet}/bin/nm-applet";
-        Restart = "always";
-      };
-      Install = {
-        WantedBy = [ "graphical-session.target" ];
-      };
-    };
+    # services.nm-applet = {
+    #   Unit = {
+    #     Description = "NetworkManager system tray applet";
+    #     After = [
+    #       "graphical-session.target"
+    #       "default.target"
+    #     ];
+    #   };
+    #   Service = {
+    #     Type = "simple";
+    #     ExecStart = "${pkgs.networkmanagerapplet}/bin/nm-applet";
+    #     Restart = "always";
+    #   };
+    #   Install = {
+    #     WantedBy = [
+    #       "graphical-session.target"
+    #       "default.target"
+    #     ];
+    #   };
+    # };
   };
 
 }
