e79ce955a159aa28840cfdc005339c92e15a8209
Henry Odza <hwodza@gmail.com>
2026-03-11 14:27:16 -0400

    changing waybar to makeoutofstoresymlink

--- user/extra_config.nix
+++ user/extra_config.nix
@@ -7,7 +7,7 @@
     ".config/neofetch/".source = ../.config/neofetch;
     # ".config/nvim/".source = ../.././.config/nvim;
     ".config/ohmyposh/".source = ../.config/ohmyposh;
-    ".config/waybar/".source = ../.config/waybar;
+    # ".config/waybar/".source = ../.config/waybar;
     ".config/rofi/".source = ../.config/rofi;
     ".config/sesh/".source = ../.config/sesh;
     ".config/tmuxinator/".source = ../.config/tmuxinator;
