f889e248eb12fc00bc296b8766c8c909293be441
Henry Odza <hwodza@gmail.com>
2026-05-14 15:06:13 -0400

    basic niri

---
+++ modules/features/niri.nix
@@ -0,0 +1,19 @@
+{ self, inputs, ... }: {
+	perSystem = { pkgs, lib, ... }: {
+		packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap { 
+			inherit pkgs;
+			settings = {
+				xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
+
+				input.keyboard.xkb.layout = "us,ua";
+
+				layout.gaps = 5;
+
+				binds = {
+				  "Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
+				  "Mod+Q".close-window = null;
+				};
+			};
+		};
+	};
+}
