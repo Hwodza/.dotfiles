1457e72628202505c242728941fe78c552fcc99d
Henry Odza <hwodza@gmail.com>
2026-05-27 13:47:24 -0400

    formated tester configuration.nix

--- modules/hosts/tester/configuration.nix
+++ modules/hosts/tester/configuration.nix
@@ -1,143 +1,146 @@
-{ self, inputs, ... }: {
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.nixosModules.testerConfiguration =
+    # Edit this configuration file to define what should be installed on
+    # your system.  Help is available in the configuration.nix(5) man page
+    # and in the NixOS manual (accessible by running ‘nixos-help’).
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }: {
+      imports = [
+        # Include the results of the hardware scan.
+        self.nixosModules.testerHardware
+        self.nixosModules.niri
+        self.nixosModules.base
+        self.nixosModules.general
+        self.nixosModules.desktop
+      ];
 
-  flake.nixosModules.testerConfiguration = 
-  	# Edit this configuration file to define what should be installed on
-	# your system.  Help is available in the configuration.nix(5) man page
-	# and in the NixOS manual (accessible by running ‘nixos-help’).
+      nix.settings.experimental-features = ["nix-command" "flakes"];
 
-	{ config, pkgs, lib, ... }:
+      # Bootloader.
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
 
-	{
-	  imports =
-	    [ # Include the results of the hardware scan.
-	      self.nixosModules.testerHardware
-	      self.nixosModules.niri
-		self.nixosModules.base
-		self.nixosModules.general
-		self.nixosModules.desktop
-	    ];
+      networking.hostName = "nixos"; # Define your hostname.
+      # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-	  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+      # Configure network proxy if necessary
+      # networking.proxy.default = "http://user:password@proxy:port/";
+      # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-	  # Bootloader.
-	  boot.loader.systemd-boot.enable = true;
-	  boot.loader.efi.canTouchEfiVariables = true;
+      # Enable networking
+      networking.networkmanager.enable = true;
 
-	  networking.hostName = "nixos"; # Define your hostname.
-	  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+      # Set your time zone.
+      time.timeZone = "America/New_York";
 
-	  # Configure network proxy if necessary
-	  # networking.proxy.default = "http://user:password@proxy:port/";
-	  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+      # Select internationalisation properties.
+      i18n.defaultLocale = "en_US.UTF-8";
 
-	  # Enable networking
-	  networking.networkmanager.enable = true;
+      i18n.extraLocaleSettings = {
+        LC_ADDRESS = "en_US.UTF-8";
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
+      };
 
-	  # Set your time zone.
-	  time.timeZone = "America/New_York";
+      # Enable the X11 windowing system.
+      services.xserver.enable = true;
 
-	  # Select internationalisation properties.
-	  i18n.defaultLocale = "en_US.UTF-8";
+      # Enable the GNOME Desktop Environment.
+      # services.xserver.displayManager.gdm.enable = true;
+      # services.xserver.desktopManager.gnome.enable = true;
 
-	  i18n.extraLocaleSettings = {
-	    LC_ADDRESS = "en_US.UTF-8";
-	    LC_IDENTIFICATION = "en_US.UTF-8";
-	    LC_MEASUREMENT = "en_US.UTF-8";
-	    LC_MONETARY = "en_US.UTF-8";
-	    LC_NAME = "en_US.UTF-8";
-	    LC_NUMERIC = "en_US.UTF-8";
-	    LC_PAPER = "en_US.UTF-8";
-	    LC_TELEPHONE = "en_US.UTF-8";
-	    LC_TIME = "en_US.UTF-8";
-	  };
+      # Configure keymap in X11
+      services.xserver.xkb = {
+        layout = "us";
+        variant = "";
+      };
 
-	  # Enable the X11 windowing system.
-	  services.xserver.enable = true;
+      # Enable CUPS to print documents.
+      services.printing.enable = true;
 
-	  # Enable the GNOME Desktop Environment.
-	  # services.xserver.displayManager.gdm.enable = true;
-	  # services.xserver.desktopManager.gnome.enable = true;
+      # Enable sound with pipewire.
+      services.pulseaudio.enable = false;
+      security.rtkit.enable = true;
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        #jack.enable = true;
 
-	  # Configure keymap in X11
-	  services.xserver.xkb = {
-	    layout = "us";
-	    variant = "";
-	  };
+        # use the example session manager (no others are packaged yet so this is enabled by default,
+        # no need to redefine it in your config for now)
+        #media-session.enable = true;
+      };
 
-	  # Enable CUPS to print documents.
-	  services.printing.enable = true;
+      # Enable touchpad support (enabled default in most desktopManager).
+      # services.xserver.libinput.enable = true;
 
-	  # Enable sound with pipewire.
-	  services.pulseaudio.enable = false;
-	  security.rtkit.enable = true;
-	  services.pipewire = {
-	    enable = true;
-	    alsa.enable = true;
-	    alsa.support32Bit = true;
-	    pulse.enable = true;
-	    # If you want to use JACK applications, uncomment this
-	    #jack.enable = true;
+      # Define a user account. Don't forget to set a password with ‘passwd’.
+      # users.users.henry = {
+      #   isNormalUser = true;
+      #   description = "henry";
+      #   extraGroups = [ "networkmanager" "wheel" ];
+      #   packages = with pkgs; [
+      #       neovim
+      #       git
+      #   #  thunderbird
+      #   ];
+      # };
 
-	    # use the example session manager (no others are packaged yet so this is enabled by default,
-	    # no need to redefine it in your config for now)
-	    #media-session.enable = true;
-	  };
+      # Install firefox.
+      programs.firefox.enable = true;
 
-	  # Enable touchpad support (enabled default in most desktopManager).
-	  # services.xserver.libinput.enable = true;
+      # Allow unfree packages
+      nixpkgs.config.allowUnfree = true;
 
-	  # Define a user account. Don't forget to set a password with ‘passwd’.
-	  # users.users.henry = {
-	  #   isNormalUser = true;
-	  #   description = "henry";
-	  #   extraGroups = [ "networkmanager" "wheel" ];
-	  #   packages = with pkgs; [
-	  #       neovim
-	  #       git
-	  #   #  thunderbird
-	  #   ];
-	  # };
+      # List packages installed in system profile. To search, run:
+      # $ nix search wget
+      environment.systemPackages = with pkgs; [
+        #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+        #  wget
+      ];
 
-	  # Install firefox.
-	  programs.firefox.enable = true;
+      # Some programs need SUID wrappers, can be configured further or are
+      # started in user sessions.
+      # programs.mtr.enable = true;
+      # programs.gnupg.agent = {
+      #   enable = true;
+      #   enableSSHSupport = true;
+      # };
 
-	  # Allow unfree packages
-	  nixpkgs.config.allowUnfree = true;
+      # List services that you want to enable:
 
-	  # List packages installed in system profile. To search, run:
-	  # $ nix search wget
-	  environment.systemPackages = with pkgs; [
-	  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-	  #  wget
-	  ];
+      # Enable the OpenSSH daemon.
+      # services.openssh.enable = true;
 
-	  # Some programs need SUID wrappers, can be configured further or are
-	  # started in user sessions.
-	  # programs.mtr.enable = true;
-	  # programs.gnupg.agent = {
-	  #   enable = true;
-	  #   enableSSHSupport = true;
-	  # };
+      # Open ports in the firewall.
+      # networking.firewall.allowedTCPPorts = [ ... ];
+      # networking.firewall.allowedUDPPorts = [ ... ];
+      # Or disable the firewall altogether.
+      # networking.firewall.enable = false;
 
-	  # List services that you want to enable:
-
-	  # Enable the OpenSSH daemon.
-	  # services.openssh.enable = true;
-
-	  # Open ports in the firewall.
-	  # networking.firewall.allowedTCPPorts = [ ... ];
-	  # networking.firewall.allowedUDPPorts = [ ... ];
-	  # Or disable the firewall altogether.
-	  # networking.firewall.enable = false;
-
-	  # This value determines the NixOS release from which the default
-	  # settings for stateful data, like file locations and database versions
-	  # on your system were taken. It‘s perfectly fine and recommended to leave
-	  # this value at the release version of the first install of this system.
-	  # Before changing this value read the documentation for this option
-	  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	  system.stateVersion = "25.11"; # Did you read the comment?
-
-	};
-
+      # This value determines the NixOS release from which the default
+      # settings for stateful data, like file locations and database versions
+      # on your system were taken. It‘s perfectly fine and recommended to leave
+      # this value at the release version of the first install of this system.
+      # Before changing this value read the documentation for this option
+      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+      system.stateVersion = "25.11"; # Did you read the comment?
+    };
 }
