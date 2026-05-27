984d956eedba0ec2e7b27f4b171c03fdc6dd3eea
Henry <hwodza@gmail.com>
2026-05-27 14:05:01 -0400

    workspace2d overlay

--- modules/hypr/hyprland.nix
+++ modules/hypr/hyprland.nix
@@ -1,12 +1,21 @@
 {
   inputs,
-  self,
   ...
 }: {
-  flake.nixosModules.hypr = {pkgs, lib, ...}: {
+  flake.nixosModules.hypr = {pkgs, ...}: {
+    nixpkgs.overlays = [
+      (_final: prev: {
+        hyprland-workspace2d = inputs.hyprland-workspace2d.packages.${prev.system}.workspace2d;
+      })
+    ];
+
     programs.hyprland = {
       enable = true;
       package = pkgs.hyprland;
     };
+
+    environment.systemPackages = [
+      pkgs.hyprland-workspace2d
+    ];
   };
 }
