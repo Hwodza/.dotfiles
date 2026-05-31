cc581dea91cb19158177c9db8f456c65d6e825b3
Henry Odza <hwodza@gmail.com>
2026-05-31 19:38:53 -0400

    added rofi

--- modules/features/desktop.nix
+++ modules/features/desktop.nix
@@ -1,5 +1,17 @@
 {self, ...}: {
-  flake.nixosModules.desktop = {pkgs, ...}: let
+  flake.homeModules.desktop = {pkgs, ...}: {
+    home.packages = [
+      pkgs.rofi
+    ];
+
+    home.file.".config/rofi/config.rasi".source = ../hypr/rofi.config.rasi;
+  };
+
+  flake.nixosModules.desktop = {
+    config,
+    pkgs,
+    ...
+  }: let
     selfpkgs = self.packages."${pkgs.system}";
   in {
     imports = [
@@ -7,6 +19,10 @@
       {
         my.hyprland.package = self.packages.${pkgs.system}.myHyprland;
       }
+    ];
+
+    home-manager.users.${config.preferences.user.name}.imports = [
+      self.homeModules.desktop
     ];
 
     environment.systemPackages = with pkgs; [
@@ -49,6 +65,5 @@
       LC_TELEPHONE = "en_US.UTF-8";
       LC_TIME = "en_US.UTF-8";
     };
-
   };
 }
