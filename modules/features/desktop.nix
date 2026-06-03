496738a43e6c85d0752569499cd0a4e7243dbab6
Henry Odza <hwodza@gmail.com>
2026-06-03 17:15:40 -0400

    added some flags to tuigreet

--- modules/features/desktop.nix
+++ modules/features/desktop.nix
@@ -44,7 +44,7 @@
         enable = true;
         settings = {
           default_session = {
-            command = "${pkgs.tuigreet}/bin/tuigreet --cmd start-hyprland";
+            command = "${pkgs.tuigreet}/bin/tuigreet --time --user-menu --cmd start-hyprland";
           };
         };
       };
