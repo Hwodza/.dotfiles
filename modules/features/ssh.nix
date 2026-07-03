b6f1a8334d534a9f2ca6b2db115bd0211e19ff53
Henry Odza <hwodza@gmail.com>
2026-07-02 23:41:10 -0400

    added some more ssh config

--- modules/features/ssh.nix
+++ modules/features/ssh.nix
@@ -4,6 +4,11 @@
       startAgent = true;
       extraConfig = ''
         AddKeysToAgent yes
+        Host *
+            SetEnv TERM=xterm-256color
+        Host github.com
+          IdentityFile ~/.ssh/id_github
+          IdentitiesOnly yes
       '';
     };
   };
