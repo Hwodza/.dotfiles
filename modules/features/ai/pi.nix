4119f39da63d49c6b4fa96d2167af8ee68d71207
Henry Odza <hwodza@gmail.com>
2026-07-13 17:31:53 -0400

    mkoutofstore was acting up

--- modules/features/ai/pi.nix
+++ modules/features/ai/pi.nix
@@ -1,5 +1,7 @@
 {
   inputs,
+  self,
+  lib,
   ...
 }: {
   flake.homeModules.pi = {
@@ -7,18 +9,12 @@
     pkgs,
     ...
   }: let
-    piSettings = "${config.home.homeDirectory}/.dotfiles/modules/features/ai/pi/agent/settings.json";
-    piModels = "${config.home.homeDirectory}/.dotfiles/modules/features/ai/pi/agent/models.json";
-    piMCP = "${config.home.homeDirectory}/.dotfiles/modules/features/ai/pi/agent/mcp.json";
-    piExtensions = "${config.home.homeDirectory}/.dotfiles/modules/features/ai/pi/agent/extensions";
+    piPath = "${config.home.homeDirectory}/.dotfiles/modules/features/ai/pi";
     system = pkgs.stdenv.hostPlatform.system;
   in {
     home.packages =  [
       inputs.llm-agents.packages.${system}.pi
     ];
-    home.file.".pi/agent/settings.json".source = config.lib.file.mkOutOfStoreSymlink piSettings;
-    home.file.".pi/agent/models.json".source = config.lib.file.mkOutOfStoreSymlink piModels;
-    home.file.".pi/agent/mcp.json".source = config.lib.file.mkOutOfStoreSymlink piMCP;
-    home.file.".pi/agent/extensions".source = config.lib.file.mkOutOfStoreSymlink piExtensions;
+    home.file.".pi".source = config.lib.file.mkOutOfStoreSymlink piPath;
   };
 }
