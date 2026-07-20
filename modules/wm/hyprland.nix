b225eef168ad8938cae99fea21b5e295550bd263
Henry Odza <hwodza@gmail.com>
2026-07-20 16:44:47 -0400

    some noctalia config and added hypridle/hyprlock

--- modules/wm/hyprland.nix
+++ modules/wm/hyprland.nix
@@ -25,6 +25,8 @@
       unstablePkgs.nwg-displays
       selfPkgs.myNoctalia
       jq
+      hypridle
+      hyprlock
     ];
     home.file.".config/rofi/config.rasi".source = ./rofi.config.rasi;
     wayland.windowManager.hyprland = {
@@ -36,48 +38,4 @@
       '';
     };
   };
-  # flake.nixosModules.hypr = {
-  #   pkgs,
-  #   lib,
-  #   config,
-  #   ...
-  # }: {
-  #   options.my.hyprland.package = lib.mkOption {
-  #     type = lib.types.package;
-  #     default = pkgs.hyprland;
-  #     description = "Hyprland package to use. Override with the wrapped build.";
-  #   };
-  #
-  #   config = {
-  #     nixpkgs.overlays = [
-  #       (_final: prev: {
-  #         hyprland-workspace2d =
-  #           inputs.hyprland-workspace2d.packages.${prev.system}.workspace2d;
-  #       })
-  #     ];
-  #
-  #     programs.hyprland = {
-  #       enable = true;
-  #       package = config.my.hyprland.package;
-  #     };
-  #
-  #     environment.systemPackages = [pkgs.hyprland-workspace2d];
-  #   };
-  # };
-  #
-  # perSystem = {
-  #   pkgs,
-  #   lib,
-  #   self',
-  #   ...
-  # }: {
-  #   packages.myHyprland = inputs.wrappers.lib.wrapPackage {
-  #     inherit pkgs;
-  #     package = pkgs.hyprland;
-  #     # binName = "Hyprland";
-  #     runtimeInputs = [self'.packages.myNoctalia];
-  #     env.TERMINAL = lib.getExe self'.packages.terminal;
-  #     flags."--config" = "/home/henry/.dotfiles/modules/hypr/hyprland.lua";
-  #   };
-  # };
 }
