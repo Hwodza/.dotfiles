bf75faed2bf8c8cbc45f4cc4c884c44712808a21
Henry Odza <hwodza@gmail.com>
2026-08-12 18:41:55 -0400

    added ssh homeModule for homelab

--- modules/features/homeLab/homeLab.nix
+++ modules/features/homeLab/homeLab.nix
@@ -1,10 +1,17 @@
 {self, ...}: {
-  flake.nixosModules.homeLab = {pkgs, ...}: {
+  flake.nixosModules.homeLab = {
+    config,
+    pkgs,
+    ...
+  }: {
     imports = [
       self.nixosModules.homeLab-cloudflare
       self.nixosModules.rclone
       self.nixosModules.miniflux
       self.nixosModules.vaultwarden
+    ];
+    home-manager.users.${config.preferences.user.name}.imports = [
+      self.homeModules.ssh
     ];
   };
 
